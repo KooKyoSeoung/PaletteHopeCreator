7cc3e0c79d6bf3b5b758bce8668f74faa1390347
KooKyoSeoung <201811141@sangmyung.kr>
2026-01-06 16:49:51 +0900

    ✨ Add | FlappyBird

--- PaletteHopeCreator/Assets/Scripts/GameManager.cs
+++ PaletteHopeCreator/Assets/Scripts/GameManager.cs
@@ -1,17 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public TMP_Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
     private int score;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for (int i = 0; i < pipes.Length; ++i)
+            Destroy(pipes[i].gameObject);
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
     public void GameOver()
     {
-        Debug.Log("end");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
